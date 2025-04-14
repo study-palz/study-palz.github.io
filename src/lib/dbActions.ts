8cd5ef6c43ae7b1b7e1d54fef00ddc01a674a5c0
dominic-isaac-molina <dim@hawaii.edu>
2025-04-13 14:18:29 -1000

    Update dbActions.ts

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -4,11 +4,20 @@ import { hash } from 'bcrypt';
 import { redirect } from 'next/navigation';
 import { prisma } from './prisma';
 
+// Change Password Input type
+export type ChangePasswordInput = {
+  email: string;
+  oldpassword: string;
+  password: string;
+  confirmPassword: string;
+};
+
 // Change Password Action
-export async function changePassword(userId: number, newPassword: string) {
-  const hashed = await hash(newPassword, 10);
+export async function changePassword(input: ChangePasswordInput) {
+  const { email, password } = input;
+  const hashed = await hash(password, 10);
   await prisma.user.update({
-    where: { id: userId },
+    where: { email },
     data: { password: hashed },
   });
   redirect('/profile');
