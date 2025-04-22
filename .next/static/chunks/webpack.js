147adaf96e72ed7fa89eef7a3db5bcfebab3077b
dominic-isaac-molina <dim@hawaii.edu>
2025-04-22 10:42:02 -1000

    Fixed login issues

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4f78fe7145d345b6"; }
+/******/ 		__webpack_require__.h = function() { return "569365c51f09f0b9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/types/app/auth/signin/page.ts
+++ .next/types/app/auth/signin/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/ieatchu/Desktop/ICS 314/study-palz.github.io/src/app/auth/signin/page.tsx
+// File: /Users/dominicmolina/Desktop/github/study-palz.github.io/src/app/auth/signin/page.tsx
 import * as entry from '../../../../../src/app/auth/signin/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
