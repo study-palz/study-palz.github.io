5de1b5cce688430bb85d8342da9462a83008fc0b
dominic-isaac-molina <dim@hawaii.edu>
2025-04-24 14:26:57 -1000

    issue-27

---
+++ .next/static/chunks/app/courses/page-760a8bc7e0bb98bb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[65,47],{9076:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=9076)}),_N_E=n.O()}]);
