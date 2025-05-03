386cda3afd03bc0a9d12a92c56a19a649c5e6c01
dominic-isaac-molina <dim@hawaii.edu>
2025-05-02 15:27:09 -1000

    Update page.tsx

---
+++ src/app/courses/[code]/sessions/[sessionId]/page.tsx
@@ -0,0 +1,9 @@
+export default function SessionPage() {
+    return (
+      <div className="p-6 text-white">
+        <h1>Study Session</h1>
+        <p>Session details will go here.</p>
+      </div>
+    );
+  }
+  
