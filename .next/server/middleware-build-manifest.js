d5b45bbe917249aa5d61ccc2d943ce3a5bce9be5
Kwanho Chun <143853207+kwanhoc1@users.noreply.github.com>
2025-05-04 19:30:32 -1000

    Update Leaderboard

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-e08b29daf12453eb.js","static/chunks/fd9d1056-76b25d8a8edaf07c.js","static/chunks/117-b1ec220fe767aff8.js","static/chunks/main-app-3c06f76e57ab09ee.js"],pages:{"/_app":["static/chunks/webpack-e08b29daf12453eb.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-8bb62053ddeaaec2.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-e08b29daf12453eb.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-8bb62053ddeaaec2.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -3,7 +3,6 @@ import { prisma } from '@/lib/prisma'
 
 const DEFAULT_IMAGE_URL = '/default-profile.png'
 
-export const dynamic = 'force-dynamic'
 
 export default async function LeaderboardPage() {
   const users = await prisma.user.findMany({
