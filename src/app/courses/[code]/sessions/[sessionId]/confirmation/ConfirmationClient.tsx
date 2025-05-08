ed3f98b6e84875d55a9488b8126f73dc64ef8032
Kwanho Chun <143853207+kwanhoc1@users.noreply.github.com>
2025-05-08 06:48:40 -1000

    Update ConfirmationClient.tsx

--- src/app/courses/[code]/sessions/[sessionId]/confirmation/ConfirmationClient.tsx
+++ src/app/courses/[code]/sessions/[sessionId]/confirmation/ConfirmationClient.tsx
@@ -9,7 +9,7 @@ interface Props {
   sessionId: number;
   ownerId: number;
   initialAttendees: { id: number; name?: string; email: string }[];
-  hasMarkedAttendance: boolean;
+  hasMarkedAttendance?: boolean;
 }
 
 export default function ConfirmationClient({
